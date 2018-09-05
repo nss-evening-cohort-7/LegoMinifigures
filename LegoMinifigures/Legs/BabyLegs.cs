98fef776263b492ce5256951869912f14880abc4
nathan gonzalez <ncgonz@gmail.com>
2018-09-05 00:08:59 -0500

    moving legs

--- LegoMinifigures/Legs/BabyLegs.cs
+++ LegoMinifigures/Legs/BabyLegs.cs
@@ -25,22 +25,4 @@ namespace LegoMinifigures.Legs
             Console.WriteLine($"Baby legs kicked {minifigure.Name}, the {minifigure.Description}.");
         }
     }
-
-    class Legs
-    {
-        public int Length { get; set; } = 34;
-        public bool HasPants { get; set; } = true;
-        public string ShoeColor { get; set; }
-        public string MainColor { get; set; }
-
-        public virtual void Walk()
-        {
-            Console.WriteLine($"The {MainColor} legs walked around the office");
-        }
-
-        public virtual void Kick(Minifigure minifigure)
-        {
-            Console.WriteLine($"The legs kicked {minifigure.Name}, the {minifigure.Description}.");
-        }
-    }
 }
---
+++ LegoMinifigures/Legs/Legs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LegoMinifigures.Legs
+{
+    class Legs
+    {
+        public int Length { get; set; } = 34;
+        public bool HasPants { get; set; } = true;
+        public string ShoeColor { get; set; }
+        public string MainColor { get; set; }
+
+        public virtual void Walk()
+        {
+            Console.WriteLine($"The {MainColor} legs walked around the office");
+        }
+
+        public virtual void Kick(Minifigure minifigure)
+        {
+            Console.WriteLine($"The legs kicked {minifigure.Name}, the {minifigure.Description}.");
+        }
+    }
+}
